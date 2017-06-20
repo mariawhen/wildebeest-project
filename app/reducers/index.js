ee0221e36fc552d0ecbd1e92a47209f07b8d95a8
Maria Nguyen <mnguyen8@mail.pima.edu>
2017-06-20 14:56:39 -0700

    Clean up: remove unused reducers, imports relatedArtists reducer

--- app/reducers/index.js
+++ app/reducers/index.js
@@ -1,29 +1,24 @@
 import { combineReducers } from 'redux';
 import { routerReducer as routing } from 'react-router-redux';
-import user from '../reducers/user';
-import topic from '../reducers/topic';
-import message from '../reducers/message';
-import * as types from '../types';
+import relatedArtists from '../reducers/relatedArtists';
+// import * as types from '../types';
 
-const isFetching = (state = false, action) => {
-  switch (action.type) {
-    case types.CREATE_REQUEST:
-      return true;
-    case types.REQUEST_SUCCESS:
-    case types.REQUEST_FAILURE:
-      return false;
-    default:
-      return state;
-  }
-};
+// const isFetching = (state = false, action) => {
+//   switch (action.type) {
+//     case types.CREATE_REQUEST:
+//       return true;
+//     case types.REQUEST_SUCCESS:
+//     case types.REQUEST_FAILURE:
+//       return false;
+//     default:
+//       return state;
+//   }
+// };
 
 // Combine reducers with routeReducer which keeps track of
 // router state
 const rootReducer = combineReducers({
-  isFetching,
-  topic,
-  user,
-  message,
+  relatedArtists,
   routing
 });
 

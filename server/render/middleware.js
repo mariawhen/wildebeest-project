9ab0da7df49c983981b3ffa1b137120fd9f6fe69
Maria Nguyen <mnguyen8@mail.pima.edu>
2017-06-20 14:51:05 -0700

    Clean up: remove unused dispatch

--- server/render/middleware.js
+++ server/render/middleware.js
@@ -3,7 +3,6 @@ import createRoutes from '../../app/routes';
 import configureStore from '../../app/store/configureStore';
 import * as types from '../../app/types';
 import pageRenderer from './pageRenderer';
-import fetchDataForRoute from '../../app/utils/fetchDataForRoute';
 
 /*
  * Export render function to be used in server/config/routes.js
@@ -50,19 +49,8 @@ export default function render(req, res) {
     } else if (redirect) {
       res.redirect(302, redirect.pathname + redirect.search);
     } else if (props) {
-      // This method waits for all render component
-      // promises to resolve before returning to browser
-      store.dispatch({ type: types.CREATE_REQUEST });
-      fetchDataForRoute(props)
-        .then((data) => {
-          store.dispatch({ type: types.REQUEST_SUCCESS, data });
-          const html = pageRenderer(store, props);
-          res.status(200).send(html);
-        })
-        .catch((err) => {
-          console.error(err);
-          res.status(500).json(err);
-        });
+      const html = pageRenderer(store, props);
+      res.status(200).send(html);
     } else {
       res.sendStatus(404);
     }
